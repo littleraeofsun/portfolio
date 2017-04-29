4a8dc4f65e645719f3e88cd44a88e113c6697cf2
Rae Melton <littleraeofsun@gmail.com>
2017-04-28 23:40:24 -0500

    Angular Controller Cleanup
    Added parameter lists to controllers and removed tutorial code

--- Website/Website/Scripts/PortfolioApp.Controllers.js
+++ Website/Website/Scripts/PortfolioApp.Controllers.js
@@ -1,12 +1,14 @@
 ﻿angular.module('PortfolioApp.Controllers', [])
 
   //MENU CONTROLLER
-  .controller('menuController', function($scope){
-      $scope.temp = "temp";
-  })
+  .controller('menuController', ['$scope', '$location', function($scope, $location){
+      $scope.getClass = function (path) {
+          return ($location.path().substr(0, path.length) === path) ? 'active' : '';
+      }
+  }])
 
   //HOME CONTROLLER
-  .controller('homeController', function ($scope, portfolioAPIservice) {
+  .controller('homeController', ['$scope', 'portfolioAPIservice', function ($scope, portfolioAPIservice) {
   	document.title = "littleraeofsun: Portfolio of Rae Melton";
   	$scope.projects = [];
   	portfolioAPIservice.getFeaturedItems().then(function (response) {
@@ -27,10 +29,10 @@
   			}
   		});
   	});
-  })
+  }])
 
   //PROJECT CONTROLLER
-  .controller('projectController', function ($scope, $routeParams, portfolioAPIservice) {
+  .controller('projectController', ['$scope', '$routeParams', 'portfolioAPIservice', function ($scope, $routeParams, portfolioAPIservice) {
   	document.title = "Project Title | littleraeofsun";
   	$scope.id = $routeParams.id;
   	$scope.project = null;
@@ -54,10 +56,10 @@
   		}
   		//TODO: handle failure
   	});
-  })
+  }])
 
   //PROJECTS CONTROLLER
-  .controller('projectsController', function ($scope, $routeParams, portfolioAPIservice) {
+  .controller('projectsController', ['$scope', '$routeParams', 'portfolioAPIservice', function ($scope, $routeParams, portfolioAPIservice) {
   	document.title = "Projects | littleraeofsun";
   	$scope.id = $routeParams.id;
   	$scope.projects = [];
@@ -68,10 +70,10 @@
   		}
   		//TODO: handle failure
   	});
-  })
+  }])
 
   //ALBUM CONTROLLER
-  .controller('albumController', function ($scope, $routeParams, portfolioAPIservice) {
+  .controller('albumController', ['$scope', '$routeParams', 'portfolioAPIservice', function ($scope, $routeParams, portfolioAPIservice) {
   	document.title = "Album Title | littleraeofsun";
   	$scope.id = $routeParams.id;
   	$scope.album = null;
@@ -94,10 +96,10 @@
   	$scope.$on('$viewContentLoaded', function () {
   		$('.section-loader').fadeOut(500);
   	});
-  })
+  }])
 
   //ALBUMS CONTROLLER
-  .controller('albumsController', function ($scope, $routeParams, portfolioAPIservice) {
+  .controller('albumsController', ['$scope', '$routeParams', 'portfolioAPIservice', function ($scope, $routeParams, portfolioAPIservice) {
   	document.title = "Albums | littleraeofsun";
   	$scope.id = $routeParams.id;
   	$scope.albums = [];
@@ -113,11 +115,11 @@
   	$scope.$on('$viewContentLoaded', function () {
   		$('.section-loader').fadeOut(500);
   	});
-  })
+  }])
 
   //CONTACT CONTROLLER
-  .controller('contactController', function ($scope, portfolioAPIservice) {
-  	document.title = "Contact Me | littleraeofsun";
+  .controller('contactController', ['$scope', 'portfolioAPIservice', '$uibModal', function ($scope, portfolioAPIservice, $uibModal) {
+      document.title = "Contact Me | littleraeofsun";
 
   	//run script after view loaded
   	$scope.$on('$viewContentLoaded', function () {
@@ -126,59 +128,4 @@
   })
 
 
-  ////RACES CONTROLLER
-  //.controller('racesController', function ($scope, $http, ergastAPIservice) {
-  //	$scope.pastRaces = [];
-  //	$scope.racesList = [];
-  //	$scope.filterName = null;
-
-  //	ergastAPIservice.getRaceWinners().success(function (response) {
-  //		//Dig into the response to get the relevante data
-  //		$scope.pastRaces = response.MRData.RaceTable.Races;
-  //		ergastAPIservice.getRaces().success(function (response) {
-  //			$scope.racesList = response.MRData.RaceTable.Races;
-  //			angular.forEach($scope.pastRaces, function (race, index) {
-  //				//Push each winning driver into raceList
-  //				$scope.racesList[index].Results = race.Results;
-  //			});
-  //		});
-  //	});
-  //})
-
-
-  ////RACE CONTROLLER
-  //.controller('raceController', function ($scope, $routeParams, $http, ergastAPIservice) {
-  //	$scope.id = $routeParams.id;
-  //	$scope.raceResult = [];
-  //	$scope.qualiResult = [];
-
-
-  //	ergastAPIservice.getRaceDetails($scope.id).success(function (response) {
-  //		$scope.race = response.MRData.RaceTable.Races[0];
-  //		$scope.raceResult = response.MRData.RaceTable.Races[0].Results;
-  //	});
-
-  //	ergastAPIservice.getQualiDetails($scope.id).success(function (response) {
-  //		$scope.qualiResult = response.MRData.RaceTable.Races[0].QualifyingResults;
-  //	});
-
-  //	//MOVE THIS SHIT TO A SERVICE
-  //	$http.jsonp('http://ergast.com/api/f1/2013/' + $routeParams.id + '/results.json?callback=JSON_CALLBACK').success(function (response) {
-  //		$scope.race = response.MRData.RaceTable.Races[0];
-  //		$scope.raceResult = response.MRData.RaceTable.Races[0].Results;
-  //	}).error(function (error) {
-  //	});
-
-  //	//THIS ONE AS WELL< FOR A SEPPARATE SERVICE
-  //	$http.jsonp('http://ergast.com/api/f1/2013/' + $routeParams.id + '/qualifying.json?callback=JSON_CALLBACK').success(function (response) {
-  //		$scope.qualiResult = response.MRData.RaceTable.Races[0].QualifyingResults;
-  //	}).error(function (error) {
-  //	});
-  //})
-
-  //.controller('menuContr', function ($scope, $location) {
-  //	$scope.$on('$routeChangeSuccess', function () {
-  //		$scope.menuActive = $location.path().split("/")[1];
-  //	});
-//})
 ;
