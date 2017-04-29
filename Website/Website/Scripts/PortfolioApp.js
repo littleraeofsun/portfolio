4c0b453653a465904e2630201e41e55643b24964
Rae Melton <littleraeofsun@gmail.com>
2017-04-28 23:41:01 -0500

    Route Cleanup and Contact changed to About

--- Website/Website/Scripts/PortfolioApp.js
+++ Website/Website/Scripts/PortfolioApp.js
@@ -9,9 +9,7 @@ config(['$routeProvider', '$locationProvider', function ($routeProvider, $locati
       when("/", { templateUrl: "partials/home.html", controller: "homeController" }).
       when("/projects/:id", { templateUrl: "partials/project.html", controller: "projectController" }).
       when("/projects", { templateUrl: "partials/projects.html", controller: "projectsController" }).
-      when("/albums/:id", { templateUrl: "partials/album.html", controller: "albumController" }).
-      when("/albums", { templateUrl: "partials/albums.html", controller: "albumsController" }).
-      when("/contact", { templateUrl: "partials/contact.html", controller: "contactController" });
+      when("/about", { templateUrl: "partials/contact.html", controller: "contactController" });
 
     // use the HTML5 History API
     $locationProvider.hashPrefix('');
